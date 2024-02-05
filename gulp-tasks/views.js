b4499688afd5c52976ac3e71106620aa6f202b5a
ilsyaa <ilham.shindou@gmail.com>
2024-02-05 12:44:41 +0700

    + gulp nunjucks + gulp prettify

--- gulp-tasks/views.js
+++ gulp-tasks/views.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { paths, prod } from "../gulpfile.babel";
+import { paths } from "../gulpfile.babel";
 import gulp from "gulp";
 import include from "gulp-file-include";
 import gulpif from "gulp-if";
@@ -8,27 +8,20 @@ import replace from "gulp-replace";
 import browsersync from "browser-sync";
 import yargs from "yargs";
 import prettify from "gulp-prettify";
+import nunjucksRender from "gulp-nunjucks-render";
 
 const argv = yargs.argv,
     production = !!argv.production;
 
 gulp.task("views", () => {
     return gulp.src(paths.views.src)
+        .pipe(nunjucksRender({
+            path: 'src/views',
+        }))
         .pipe(include({
             prefix: "@@",
             basepath: "@file"
         }))
-        .pipe(replace('<!-- stack: styles -->', function handleReplace() {
-            let string = this.file.contents.toString().match(/<!-- push-start: styles -->([\s\S]*?)<!-- push-end: styles -->/);
-            if(string) return string[1].trimEnd();
-            return;
-        }))
-        .pipe(replace('<!-- stack: scripts -->', function handleReplace() {
-            let string = this.file.contents.toString().match(/<!-- push-start: scripts -->([\s\S]*?)<!-- push-end: scripts -->/);
-            if(string) return string[1].trimEnd();
-            return;
-        }))
-        .pipe(replace(/<!-- push-start: (.*) -->([\s\S]*?)<!-- push-end: (.*) -->/, ''))
         .pipe(gulpif(production, replace("/app.css", "/app.min.css")))
         .pipe(gulpif(production, replace("/app.css", "/app.min.css")))
         .pipe(gulpif(production, prettify()))
