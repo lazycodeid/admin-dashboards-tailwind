9f4a030d0be10e0a11d31b7d18080c999c40ae25
ilsyaa <ilham.shindou@gmail.com>
2024-02-25 21:03:18 +0700

    plugin ripple

--- src/js/plugins/ripple.js
+++ src/js/plugins/ripple.js
@@ -1,34 +1,44 @@
-document.querySelectorAll("[lazy-ripple]").forEach(element => {
-    element.addEventListener("mousedown", rippleEffect);
-});
+class LazyRipple {
+    constructor(el) {
+        this.el = el
+        this.#init()
+    }
 
-function rippleEffect(event) {
-    const btn = event.currentTarget;
-    let time;
-    if (time) clearTimeout(time);
+    #init(){
+        this.el.addEventListener("mousedown", this.#enter)
+    }
 
-    const circle = document.createElement("span");
-    const diameter = Math.max(btn.clientWidth, btn.clientHeight);
-    const radius = diameter / 2;
+    #enter(event){
+        const btn = event.currentTarget;
+        let time;
+        if (time) clearTimeout(time);
+        const wrapper = document.createElement("span");
+        wrapper.classList.add("ripple-wrapper")
+        const circle = document.createElement("span");
+        const diameter = Math.max(btn.clientWidth, btn.clientHeight);
+        const radius = diameter / 2;
+        const btnRect = btn.getBoundingClientRect();
+    
+        circle.style.width = circle.style.height = `${diameter}px`;
+        circle.style.left = `${event.clientX - (btnRect.left + radius)}px`;
+        circle.style.top = `${event.clientY - (btnRect.top + radius)}px`;
+        circle.classList.add("ripple");
+        wrapper.appendChild(circle);
+        btn.appendChild(wrapper);
+    
+        time = setTimeout(() => wrapper.remove(), 1000);
+    }
 
-    // Get the position of the button relative to the viewport
-    const btnRect = btn.getBoundingClientRect();
-
-    circle.style.width = circle.style.height = `${diameter}px`;
-    circle.style.left = `${event.clientX - (btnRect.left + radius)}px`;
-    circle.style.top = `${event.clientY - (btnRect.top + radius)}px`;
-    circle.classList.add("ripple");
-    btn.appendChild(circle);
-
-    time = setTimeout(() => circle.remove(), 1000);
+    static autoInit() {
+        document.querySelectorAll("[lazy-ripple]").forEach((element) => {
+            new LazyRipple(element)
+        })
+    }
 }
 
-// ripple button circle
-document.querySelectorAll(".btn-circle").forEach((element)=>{
-    const ripple_wrapper = document.createElement("span")
-    ripple_wrapper.classList.add("ripple-wrapper")
-    const createspan = document.createElement("span");
-    createspan.classList.add("ripple-circle")
-    ripple_wrapper.appendChild(createspan)
-    element.appendChild(ripple_wrapper)
+window.LazyRipple = LazyRipple
+// window.$LazyTabsCollection = new Map();
+
+window.addEventListener('load', () => {
+    LazyRipple.autoInit()
 })
